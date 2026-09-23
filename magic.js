387c45997977fae5564e30bcf65de35ba5ecb030
prasanth-naraine <mailto.lakshminarayanan.official@gmail.com>
2026-09-23 11:24:07 +0530

    removed fortune smiles folder

--- magic.js
+++ magic.js
@@ -1,4 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const preloader = document.getElementById("preloader");
-  preloader.classList.add("hide");
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const preloader = document.getElementById("preloader");
+//   preloader.classList.add("hide");
+// });
+
+
+// -------------------------------------------------------------------------
+setInterval(function () {
+  const preloader = document.querySelector('#preloader');
+  preloader.classList.add('hide');
+}, 3100);
